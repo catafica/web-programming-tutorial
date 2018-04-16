28a753f315d1c95115d7c598acfbfe78e86f4ad5
Cata <alex.fica1991@gmail.com>
2018-04-16 18:32:08 +0300

    putin Ajax, tabel

--- js/phone-book.js
+++ js/phone-book.js
@@ -1,6 +1,21 @@
-var row1 = "<tr><td>Nicolae</td><td>Matei</td><td>O</td></tr>";
-var row2 = "<tr><td>Andrei</td><td>Ilie</td><td>O</td></tr>";
+function getRow(firstName, lastName, phone) {
+    return "<tr><td>" + firstName + "</td><td>" + lastName + "</td><td>" + phone + "</td></tr>";
 
-// document.getElementById('phone-book').getElementsByTagName('tbody')[0].innerHTML = row1 + row2;
+}
 
-$('#phone-book tbody').html (row1 + row2);
+var persons = [
+    ["Nicolae", "Matei", "123"],
+    ["Andrei", "Matei", "123"],
+    ["Maria", "Matei", "123"],
+    ["Valentin", "Matei", "123"]
+];
+
+var rows = '';
+
+
+for (var i = 0; i < persons.length; i++) {
+    var person = persons[i];
+    rows += getRow(person[0], person[1], person[2]);
+}
+
+$('#phone-book tbody').html(rows);
