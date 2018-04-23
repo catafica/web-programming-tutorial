96e84566d4dedcb27b97be356942b2e2b3bd507e
Cata <alex.fica1991@gmail.com>
2018-04-23 21:41:30 +0300

    modificari tabel

--- js/functii.js
+++ js/functii.js
@@ -17,14 +17,14 @@ function show_prime_numbers(n) {
          i++;
      }
 }
-function numereprime(n) {
-    var i = 2;
-    for (; i < n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    } return true;
-}
+// function numereprime(n) {
+//     var i = 2;
+//     for (; i < n; i++) {
+//         if (n % i == 0) {
+//             return false;
+//         }
+//     } return true;
+// }
 function numereprime(n) {
     var i = 2;
     for (; i < n; i++) {
@@ -36,4 +36,42 @@ function numereprime(n) {
     }
     console.warn('not found');
     return true;
+};
+
+function aduna() {
+    total = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+    }
+    return total;
 }
+
+function divide3(n) {
+    var i = 3;
+    for (; i <= n; i = (i + 3)) {
+        if (n % i == 0) {
+            // console.info('i', i);
+            return true;
+        }
+    }
+    return false;
+};
+
+function divide5(n) {
+    if  ( n % 5 == 0) {
+        return true;
+    }
+    return false;
+};
+
+function shownumbers(m) {
+    var i = 1;
+    for (;i <= m; i++) {
+        if (divide3(i) && divide5(i)) {
+            console.log('buzzfeed'); }
+            else if (divide3(i)) { console.log('buzz'); }
+            else if (divide5(i)) { console.log('feed'); }
+            else { console.log(i); }
+    }
+};
+
--- js/phone-book.js
+++ js/phone-book.js
@@ -1,21 +1,58 @@
-function getRow(firstName, lastName, phone) {
-    return "<tr><td>" + firstName + "</td><td>" + lastName + "</td><td>" + phone + "</td></tr>";
+function getRow(firstName, lastName, phone, id) {
+    return '<tr>' +
+        '<td>' + firstName + '</td>' +
+        '<td>' + lastName + '</td>' +
+        '<td>' + phone + '</td>' +
+        `<td><a href="tmp/remove-contact.html?id=${id}"> x</a></td>` +
+        '</tr>';
 
 }
 
-var persons = [
-    ["Nicolae", "Matei", "123"],
-    ["Andrei", "Matei", "123"],
-    ["Maria", "Matei", "123"],
-    ["Valentin", "Matei", "123"]
-];
+var persons = [];
 
-var rows = '';
+$.get('js/mocks/phone-book.json').done(function(data) {
+    console.warn("phone book loaded");
+    persons = data;
+    display();
+});
+
+console.info('should load persons');
 
 
-for (var i = 0; i < persons.length; i++) {
-    var person = persons[i];
-    rows += getRow(person[0], person[1], person[2]);
+function display() {
+    var rows = '';
+
+    function createRows(person) {
+        rows += getRow(person.firstName, person.lastName, person.phone, person.id ) ;
+    }
+    persons.forEach(createRows);
+
+    rows += '<tr>' +
+        '<td><input type="text" required name="firstName" placeholder="Enter first name"></td>' +
+        '<td><input type="text" name="lastName" placeholder="Enter last name"></td>' +
+        '<td>' +
+            '<input type="text" name="phone" placeholder="Enter phone">' +
+            '<button type="submit">Add</button>' +
+        '</td>' +
+        '</tr>';
+
+
+
+
+
+    $('#phone-book tbody').html(rows);
 }
 
-$('#phone-book tbody').html(rows);
+
+
+// for (var i = 0; i < persons.length; i++) {
+//     var person = persons[i];
+//     rows += getRow(person[0], person[1], person[2]);
+
+
+// $('#phone-book tbody').html(rows);
+
+
+
+
+
